5d120b9ba1f4da86b9d3ef3126b120a1d45da17d
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-05-22 22:42:57 +0530

    Update routes.py

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -8,19 +8,19 @@ from ..funcs import admin_only
 admin = Blueprint("admin", __name__, url_prefix="/admin", static_folder="static", template_folder="templates")
 
 @admin.route('/')
-
+@admin_only
 def dashboard():
     orders = Order.query.all()
     return render_template("admin/home.html", orders=orders)
 
 @admin.route('/items')
-
+@admin_only
 def items():
     items = Item.query.all()
     return render_template("admin/items.html", items=items)
 
 @admin.route('/add', methods=['POST', 'GET'])
-
+@admin_only
 def add():
     form = AddItemForm()
 
@@ -40,7 +40,7 @@ def add():
     return render_template("admin/add.html", form=form)
 
 @admin.route('/edit/<string:type>/<int:id>', methods=['POST', 'GET'])
-
+@admin_only
 def edit(type, id):
     if type == "item":
         item = Item.query.get(id)
@@ -72,7 +72,7 @@ def edit(type, id):
     return render_template('admin/add.html', form=form)
 
 @admin.route('/delete/<int:id>')
-
+@admin_only
 def delete(id):
     to_delete = Item.query.get(id)
     db.session.delete(to_delete)
