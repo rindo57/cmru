8e3940c9efa3fc51aac5ea87e451409af5d3345e
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-05-22 22:03:44 +0530

    Update routes.py

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -14,13 +14,13 @@ def dashboard():
     return render_template("admin/home.html", orders=orders)
 
 @admin.route('/items')
-@admin_only
+
 def items():
     items = Item.query.all()
     return render_template("admin/items.html", items=items)
 
 @admin.route('/add', methods=['POST', 'GET'])
-@admin_only
+
 def add():
     form = AddItemForm()
 
@@ -40,7 +40,7 @@ def add():
     return render_template("admin/add.html", form=form)
 
 @admin.route('/edit/<string:type>/<int:id>', methods=['POST', 'GET'])
-@admin_only
+
 def edit(type, id):
     if type == "item":
         item = Item.query.get(id)
@@ -72,7 +72,7 @@ def edit(type, id):
     return render_template('admin/add.html', form=form)
 
 @admin.route('/delete/<int:id>')
-@admin_only
+
 def delete(id):
     to_delete = Item.query.get(id)
     db.session.delete(to_delete)
