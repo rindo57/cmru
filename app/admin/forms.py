e1a403874ed8fbd49e3387e10d8b0fe66a89e4ef
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-05-22 23:37:17 +0530

    Update forms.py

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -10,7 +10,7 @@ class AddItemForm(FlaskForm):
 	image = FileField("Image:", validators=[DataRequired()])
 	details = StringField("Details:", validators=[DataRequired()])
 	submit = SubmitField("Add")
-
+        price_id = "".join([random.choice(digits) for n in range(5)])
 class OrderEditForm(FlaskForm):
 	status = StringField("Status:", validators=[DataRequired()])
 	submit = SubmitField("Update")
